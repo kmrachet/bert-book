acf603c4f6649fab6ea2e6fb28a02b28e1fbedf3
kmrachet <kimura.michito@ncvc.go.jp>
2023-06-23 14:02:42 +0900

    Dockerの設定ファイルを更新

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,1 +1,9 @@
-docker container run -dit -p 8999:8888 -e WEBSOCKET_CLIENT_CA_BUNDLE=/etc/ssl/certs/NCVC-SSL-Decryption-CA-trust.pem --rm --gpus all --name bert-book --mount type=bind,source=$(pwd)/../,target=/workspace/bert-book kimura/pytorch:bert-book
+#!/bin/bash
+
+docker container run \
+  -dit \
+  --rm \
+  --gpus all \
+  --name bert-book \
+  --mount type=bind,source=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../,target=/workspace/bert-book \
+  kimura/pytorch:bert-book
